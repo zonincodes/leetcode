374db04c5bc193c5aac269772933d8260f3f8616
zonin <business.kariukimartin@gmail.com>
2023-03-15 23:17:55 +0300

    polymorphism explanation

---
+++ polymorphism.cpp
@@ -0,0 +1,3 @@
+//  Polymorphism refers to the ability of acquiring many forms.
+// In the context of OOP this measns that the same function name denotes many functions that are members of difeernt objects.
+
