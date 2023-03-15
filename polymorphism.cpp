29033bbfb7f90952c2db486630f8702f70e1667e
zonin <business.kariukimartin@gmail.com>
2023-03-15 23:26:52 +0300

    main function populate

--- polymorphism.cpp
+++ polymorphism.cpp
@@ -8,7 +8,19 @@ class Class2;
 class Class3;
 int main()
 {
-
+    Class1 object1, *p;
+    Class2 object2;
+    Class3 object3;
+    p = &object1;
+    p->f(); 
+    // p->g(); 
+    p = (Class1 *)&object2;
+    p->f();
+    // p->g();
+    p = (Class1 *)&object3;
+    p->f(); // possibly abnormal program termination;
+    // p->g();
+    // p->h(); // h() is not a member of Class1;
     return 0;
 }
 
