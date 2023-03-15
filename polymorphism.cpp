e1c33d044de5deca164438d02fa48e51559976ec
zonin <business.kariukimartin@gmail.com>
2023-03-15 23:20:55 +0300

    class1 defination

--- polymorphism.cpp
+++ polymorphism.cpp
@@ -1,5 +1,7 @@
 //  Polymorphism refers to the ability of acquiring many forms.
 // In the context of OOP this measns that the same function name denotes many functions that are members of difeernt objects.
+#include <iostream>
+using namespace std;
 
 class Class1;
 
@@ -8,3 +10,11 @@ int main()
 
     return 0;
 }
+
+
+class Class1 {
+    public: 
+        virtual void f() {
+            cout << "Function f() in Class1\n" ;
+        }
+};
