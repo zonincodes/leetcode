26dad6564bd838de8214cd94287564e3af3850da
zonin <business.kariukimartin@gmail.com>
2023-03-16 07:12:07 +0300

    target sum

---
+++ target_sum.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> twoSum(vector<int> &nums, int target)
+    {
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int pointer = nums[i];
+            cout << "Pointer : " << pointer << endl;
+            for (int j = 1; j < nums.size(); j++)
+            {
+                cout << "nums[j]" << nums[j] << endl;
+                if ((pointer + nums[j]) == target)
+                {
+                    return {i, j};
+                }
+            }
+        }
+        return {};
+    }
+};
+
+int main(){
+    Solution one;
+    vector<int> test_case_one = {2, 3};
+    vector<int> test_case_1  = {0, 1};
+
+    if(one.twoSum(test_case_1, 5) == test_case_1){
+        cout << "True" << endl;
+    }  else {
+        cout <<"False \n";
+    }
+}
