61eb19144777c7886c3186c98c51df4d7b754ec5
zonin <business.kariukimartin@gmail.com>
2023-03-16 17:26:46 +0300

    sort vectors

--- vector_overloading/vector_overloading.cpp
+++ vector_overloading/vector_overloading.cpp
@@ -2,40 +2,65 @@
 #include <vector>
 #include <stdlib.h>
 #include <string.h>
+#include <algorithm>
 
 using namespace std;
 
-class Person{};
 
-int main (){
 
-    return 0;
-}
 
 class Person {
     public:
-        Person(char *n = "", int a = 0){
-            name = strdup(n);
-            age = a;
+        Person(string n = "", int a = 0) : name(new char[n.length() + 1]), age(a)
+        {
+            strcpy(name, n.c_str());
         }
 
-        ~Person() {
-            free(name);
+        string get_name(){
+            return name;
         }
 
-        bool operator==(const Person &p) const {
+        int get_age(){
+            return age;
+        }
+
+        // ~Person() {
+        //     free(name);
+        // }
+
+        bool operator==(const Person& p) const {
             return strcmp(name, p.name) == 0 && age == p.age;
         }
 
-        bool operator<(const Person &p) const {
-            return !(*this == p) && !(*this < p);
+        bool operator<(const Person& p) const {
+            return strcmp(name, p.name) < 0;
         }
 
-        bool operator>(const Person &p) const {
+        bool operator>(const Person& p) const {
             return !(*this == p) && !(*this < p);
         }
 
     private:
         char *name;
         int age;
+        friend bool lesserAge(const Person &, const Person &);
 };
+
+int main()
+{
+        // declaration
+        vector<Person> v6(1, Person("Gregg", 25));
+
+        // add more objects
+        v6.push_back(Person("Ann", 30));
+        v6.push_back(Person("Bill", 20));
+
+        //  executing
+        sort(v6.begin(), v6.end());
+
+        for (Person p : v6)
+        {
+            cout << p.get_name() << " " << p.get_age() << endl;
+        }
+        return 0;
+}
